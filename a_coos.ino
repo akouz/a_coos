704e58402163902d8ec186ffce50866acb2a56fc
Alex Kouznetsov <akouz@users.noreply.github.com>
2019-03-15 07:51:32 +1030

    Add files via upload

--- a_coos.ino
+++ a_coos.ino
@@ -1,26 +1,40 @@
 /*
  * Sketch    a_coos.ino - simple demo for cooperative OS named COOS
  * Author    A.Kouznetsov
- * Rev       1.2 dated 14/07/2018
- * Target    tested with Arduino Pro Mini, RAK Creator and NodeMCU
+ * Rev       1.3 dated 14/03/2019
 
-Redistribution and use in source and binary forms, with or without modification, are permitted.
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR 
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ */
 
 #include "coos.h"
+
+//##############################################################################
+// Def
+//##############################################################################
+
+#ifndef BUILTIN_LED
+  #define BUILTIN_LED 13
+#endif  
 
 //##############################################################################
 // Var
 //##############################################################################
 
 static int led = BUILTIN_LED;
+
+Coos <4> coos;       // create operating system with up to 4 tasks  
 
 //##############################################################################
 // Func
@@ -66,6 +80,7 @@ void coos_task1(void)
 void setup()
 {
   Serial.begin(115200);
+  Serial.println();
   Serial.println("== COOS demo ==");
   pinMode(led, OUTPUT);
   coos.register_task(coos_task0);    
--- coos.h
+++ coos.h
@@ -1,32 +1,37 @@
 /*
  * Library   COOS - COoperative Operating System
  * Author    A.Kouznetsov
- * Rev       1.2 dated 14/07/2018
+ * Rev       1.3 dated 14/03/2019
  * Target    Arduino
 
-Redistribution and use in source and binary forms, with or without modification, are permitted.
+Redistribution and use in source and binary forms, with or without modification, 
+are permitted.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR 
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
  
 #ifndef __COOS_H
 #define __COOS_H
 
-//###########################################################
+//##############################################################################
 // Inc
-//###########################################################
+//##############################################################################
 
 #include <Arduino.h>
 #include <setjmp.h>
 
-//###########################################################
+//##############################################################################
 // Type                                                   
-//###########################################################
+//##############################################################################
 
 #ifndef __UCHAR_DEFINED__
   #define __UCHAR_DEFINED__
@@ -37,19 +42,18 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   typedef signed   long slong;
 #endif
 
-//###########################################################
+//##############################################################################
 // Def                                              
-//###########################################################
+//##############################################################################
 
-#define COOS_MAX_TASKS       8     // max number of tasks, set as many as required (up to 255)
 #define COOS_STOP            -2    // set this value to stop task
-#define COOS_DELAY(__delay)  if (!setjmp(coos.task_context[coos.task_no])) {longjmp(coos.main_context, __delay+1);} else
+#define COOS_DELAY(__delay)  if (!setjmp(coos.task_context[coos.task_no])) {longjmp(coos.main_context, __delay+1);} else{}
 
-//###########################################################
-// Class                                              
-//###########################################################
+//##############################################################################
+// Template class                                              
+//##############################################################################
 
-class Coos{
+template <unsigned char COOS_MAX_TASKS> class Coos{
   public:
                 Coos(void);                           // constructor
     void        register_task(void (*tsk)(void));     // user tasks must be registered first
@@ -67,6 +71,113 @@ class Coos{
     uint        ms;
     uchar       task_cnt;                             // counts registered coos tasks
 };
-extern Coos coos;
 
+//##############################################################################
+// Implementation
+//##############################################################################
+
+// =================================
+// Constructor
+// =================================
+template <unsigned char COOS_MAX_TASKS> Coos<COOS_MAX_TASKS>::Coos(void)
+{
+  uint i;
+  uptime = 0;
+  msec = 0;
+  ms = 0;
+  task_cnt = 0;
+  for (i=0; i<COOS_MAX_TASKS; i++)
+  {
+    tsk_p[i] = NULL;                   // task is not registered
+    task_delay[i] = COOS_STOP;         // all unregistered tasks stopped
+  }
+}
+// =================================
+// Register a task 
+// =================================
+template <unsigned char COOS_MAX_TASKS> void Coos<COOS_MAX_TASKS>::register_task(void (*tsk)(void))
+{
+  if (task_cnt < COOS_MAX_TASKS)
+  {
+    tsk_p[task_cnt++] = tsk;
+  }
+}
+// =================================
+// Update time
+// =================================
+// supposed to happen more often than every millisecond,
+// but can be late if a task takes long time to execute
+template <unsigned char COOS_MAX_TASKS> void Coos<COOS_MAX_TASKS>::update_time(void)
+{
+  uint millisec = (uint)millis(); 
+  while (ms != millisec) // catch up time
+  {      
+    ms++;  
+    for (int i=0; i<task_cnt; i++)
+    {
+      if (task_delay[i] > 0)  // positive delays 
+      {
+        task_delay[i]--;
+      }
+    }
+    if (++msec >= 1000) // if 1 sec passed
+    {
+      uptime++;         // count seconds since start
+      msec = 0;
+    }
+  }  
+}
+// =================================
+// Start scheduler - init registered tasks
+// =================================
+template <unsigned char COOS_MAX_TASKS> void Coos<COOS_MAX_TASKS>::start(void)
+{
+  int   res;
+  void (*tsk)(void);
+  update_time(); 
+  for (task_no=0; task_no<task_cnt; task_no++)
+  {
+    if (tsk_p[task_no] != NULL)            // if task was registered
+    {
+      res = setjmp(main_context);
+      if (res == 0)
+      {
+        tsk = tsk_p[task_no];
+        tsk();                             // invoke task
+      }
+      else
+      {
+        task_delay[task_no] = --res;      // task returns the required delay
+      }
+    }  
+    update_time(); 
+  }
+  task_no = 0;
+}
+// =================================
+// Run scheduler on regular basis
+// =================================
+template <unsigned char COOS_MAX_TASKS> void Coos<COOS_MAX_TASKS>::run(void)
+{
+  int res;
+  int tmp;
+  if (task_delay[task_no] == 0)
+  {
+    res = setjmp(main_context);           // set return point and get delay value from the task 
+    if (res == 0)                         // after setting return point
+    {
+      longjmp(task_context[task_no], 1);  // invoke task 
+    }
+    else                                  // after returning from task 
+    {
+      tmp = --res;
+      task_delay[task_no] = tmp;          // set task delay (negative delay - task stopped) 
+    }
+  }
+  if (++task_no > task_cnt)               // next task
+  {
+    task_no = 0;                          // round-robin
+  }
+  update_time(); 
+}
 #endif /* __COOS_H */
